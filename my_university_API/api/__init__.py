cee9301c3fe8a94d92db990e43345ebfc903837b
Roberto <vecchioroberto93@gmail.com>
2020-05-03 18:36:41 +0200

    api module
    - update of .flaskenv, now we are in development mode
    - created api folder/module
    - created __init__.py
    - coded __init__.py
    - created routes.py
    - coded routes.py

---
+++ my_university_API/api/__init__.py
@@ -0,0 +1,7 @@
+# This file is executed when the api module is imported
+from flask import Flask  # to use flask Framework
+
+# creation of a Flask object
+app = Flask(__name__)
+
+# TODO: from api import routes
