71cebc8652af8a12587a6c3ba290a0549ca380bd
Roberto <vecchioroberto93@gmail.com>
2020-05-26 10:16:16 +0200

    init of index
    - created index.js
    - created app.js

---
+++ my_university_pwa/src/components/App.js
@@ -0,0 +1,14 @@
+//import lib
+import React, {Component} from 'react';
+
+//create a component 
+class App extends Component {
+    render(){
+        return (
+            <div>App component</div>
+        );
+    }
+}
+
+//export a component
+export default App;
