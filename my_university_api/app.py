d04b2360bf3de378651486bf428f0dee7e6cd676
Carlo Lomello <carlo.lomello001@studenti.uniparthenope.it>
2020-06-01 14:44:23 +0200

    added navigation at run

--- my_university_api/app.py
+++ my_university_api/app.py
@@ -3,7 +3,19 @@ app = createApp()
 
 @app.route('/')
 def home():
-    return "Homepage"
+    return """
+    <body>
+        <button> <a id='chat_button'> chat </a> </button>
+        <button> <a id='api_button'> api </a> </button>
+    </body>
+    
+    <script>
+        let chat_address = window.location + '/chat';
+        let api_address = window.location + '/api';        
+        document.getElementById("chat_button").setAttribute("href", chat_address);
+        document.getElementById("api_button").setAttribute("href", api_address);
+    </script>
+    """
 
 
 if __name__ == '__main__':
