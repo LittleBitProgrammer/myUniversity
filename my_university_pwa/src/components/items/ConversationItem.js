2c67a0757210c94b983d1c48a6e2e2d328a61130
Roberto <vecchioroberto93@gmail.com>
2020-06-10 11:38:42 +0200

    fix
    minor fix

--- my_university_pwa/src/components/items/ConversationItem.js
+++ my_university_pwa/src/components/items/ConversationItem.js
@@ -1,19 +1,15 @@
 
 // IMPORT LIB
 import React from "react";
-import Card from "../bootstrap/Card/Card";
-
 
 
 // CREATE A COMPONENT
 
-const ConversationItem = (message)=>{
-    console.log(message)
-    const mex = message.message;
+const ConversationItem = ({message,time,isUserLogin})=>{
     return (
-        <Card>
-            <p>{mex.messaggio}</p>
-        </Card>
+        <div>
+            <p className={isUserLogin ? 'chat-right' : 'chat-left'}>{message}</p>
+        </div>
     )
 }
 
--- my_university_pwa/src/components/list/ConversationList.js
+++ my_university_pwa/src/components/list/ConversationList.js
@@ -1,12 +1,19 @@
 // IMPORT LIB
 import React from 'react';
-import ConversationItem from "../items/ConversationItem";
+import ConversationItem from '../items/ConversationItem';
+import {takeTime} from '../../utility/functions'
 
 // CREATE A COMPONENT
 const ConversationList = ({messages}) => {
     const messageList = messages.map((message,index) => {
+        console.log(message);
 
-        return(<ConversationItem key={index} message={message} />)
+        return(
+            <ConversationItem 
+              key={index} 
+              message={message.messaggio} 
+              time={takeTime(message.data_invio)}/>
+        )
     })
     return <div>{messageList}</div>;
 }
